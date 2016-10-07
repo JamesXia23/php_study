e4a57d9022e689aeef25e9d93ac968f1673d455e
JamesXia <806514119@qq.com>
2016-10-08 00:59:59 +0800

    完成了music页面的编写

--- java_jam/js/index.js
+++ java_jam/js/index.js
@@ -2,9 +2,15 @@ function changePage(obj){
 	if (obj.id == 'navMenu') {
 		document.getElementById('mainHome').style.display = 'none';
 		document.getElementById('mainMenu').style.display = 'inline';
+		document.getElementById('mainMusic').style.display = 'none';
 	} else if (obj.id == 'navHome') {
 		document.getElementById('mainHome').style.display = 'inline';
 		document.getElementById('mainMenu').style.display = 'none';
+		document.getElementById('mainMusic').style.display = 'none';
+	} else if (obj.id == 'navMusic') {
+		document.getElementById('mainHome').style.display = 'none';
+		document.getElementById('mainMenu').style.display = 'none';
+		document.getElementById('mainMusic').style.display = 'inline';
 	}
 	
 }
